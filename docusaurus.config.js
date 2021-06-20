8a8d66a51c609f0f154da05ca32056d9a66a3e17
Mohammad Zulfikar <48849323+mohzulfikar@users.noreply.github.com>
2021-06-20 22:38:39 +0700

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ module.exports = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'porosub', // Usually your GitHub org/user name.
   projectName: 'study-group', // Usually your repo name.
   themeConfig: {
     navbar: {
