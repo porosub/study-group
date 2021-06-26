a2d776ec9ad87f6c8fef572a4c395a51694c2085
Mohammad Zulfikar <48849323+mohzulfikar@users.noreply.github.com>
2021-06-27 00:06:09 +0700

    Fix broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro-cys">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/cys/intro">
+            Overview - 5min ⏱️
           </Link>
         </div>
       </div>
